3539230c0ad2d33870fbdba283ab27764d2b4ca1
H0jinPark <akskalxj12@gmail.com>
2026-03-18 11:48:10 +0900

    v2 -> v3_small로 모델 변경

--- model.py
+++ model.py
@@ -4,7 +4,7 @@ from torchvision import models
 
 def get_model(num_classes=31):
     # 1. 깃털처럼 가벼운 MobileNetV2 등판!
-    model = models.mobilenet_v2(weights=models.MobileNet_V2_Weights.DEFAULT)
+    model = models.mobilenet_v3_small(weights=models.MobileNet_V3_Small_Weights.DEFAULT)
     
     # 2. 뇌 이식 수술 (주의: ResNet이랑 구조가 살짝 다릅니다!)
     # ResNet은 마지막 층 이름이 'fc'였지만, 
@@ -16,4 +16,4 @@ def get_model(num_classes=31):
 
 if __name__ == "__main__":
     my_model = get_model()
-    print("MobileNetV2 이식 완료! 깃털처럼 가볍습니다 ㅋㅋㅋ")
+    print("MobileNetV3 Small 이식 완료! 깃털처럼 가볍습니다 ㅋㅋㅋ")
